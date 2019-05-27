9338ad28273e338f487947833c58432d2d926aa2
Luis Osvaldo <os@MacBook-Pro-de-Luis.local>
2019-05-27 10:33:16 -0500

    Updates

---
+++ precache-manifest.c4c00bd37c4342342ea3a0f733e9d1dd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8cd074793a34fdbb72c87774c827e4e",
+    "url": "/TBA/index.html"
+  },
+  {
+    "revision": "d19b274ce2e8acd8e27f",
+    "url": "/TBA/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "8fd526e8b8be1a7794a2",
+    "url": "/TBA/static/css/main.8eb65855.chunk.css"
+  },
+  {
+    "revision": "d19b274ce2e8acd8e27f",
+    "url": "/TBA/static/js/2.9089bbae.chunk.js"
+  },
+  {
+    "revision": "8fd526e8b8be1a7794a2",
+    "url": "/TBA/static/js/main.49f70d95.chunk.js"
+  },
+  {
+    "revision": "42b1e7a03c633b559079",
+    "url": "/TBA/static/js/runtime~main.52b568b3.js"
+  },
+  {
+    "revision": "2eb3c329ff70ee711e86f5e9bbcb8473",
+    "url": "/TBA/static/media/chambers.2eb3c329.png"
+  },
+  {
+    "revision": "cd7d3bc1efcf100d3c735a6ca488105a",
+    "url": "/TBA/static/media/imgOficinas.cd7d3bc1.png"
+  },
+  {
+    "revision": "76ce3e1a88c12f127a34856f63f958b7",
+    "url": "/TBA/static/media/logoTuranzas.76ce3e1a.png"
+  }
+]);
